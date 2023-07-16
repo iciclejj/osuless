622ea59c54d8644dea2344d03cd2c84acb20652a
icicle <55782191+iciclejj@users.noreply.github.com>
2023-07-16 14:43:04 +0200

    Suppress tee output in install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ sudo cp "$DIR/scripts/osuless.sh" "$BIN_PATH" && sudo chmod +x "$BIN_PATH"
 # Create desktop entry
 desktop_template=$(cat "$DIR/data/osuless.desktop.template")
 desktop_entry="${desktop_template/__BIN_PATH__/$BIN_PATH}"
-echo "$desktop_entry" | sudo tee "$DESKTOP_PATH"
+echo "$desktop_entry" | sudo tee "$DESKTOP_PATH" >> /dev/null
 
 # Install custom osz mime-type
 xdg-mime install --novendor "$DIR/data/osuless.xml"
